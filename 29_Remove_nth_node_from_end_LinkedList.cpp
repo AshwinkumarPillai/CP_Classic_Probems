1e0ec2c5e83cad3b0de5096bcb307d6ce4fc3516
Ashwinkumar Pillai <ashwinfury1@gmail.com>
2020-06-10 17:09:18 +0530

    Remove kth node from end of linked list

---
+++ 29_Remove_nth_node_from_end_LinkedList.cpp
@@ -0,0 +1,18 @@
+// Remove the kth node from the end of the list;
+
+void RemoveNode(Node *head, int k)
+{
+    Node *first = head, second = head;
+    for (int i = 0; i < k; i++)
+    {
+        first = first->next;
+    }
+
+    while (first != NULL)
+    {
+        second = second->next;
+        first = first->next;
+    }
+    first->val = first->next->val;
+    first->next = first->next->next;
+}
